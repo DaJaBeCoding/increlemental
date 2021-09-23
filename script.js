a8072ed932650bb89b74ad7a384459f4b534aa50
DaJaBeCoding <38948265+DaJaBeCoding@users.noreply.github.com>
2021-09-23 23:58:02 +0200

    some click

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ var deltaT = 0;
 var energy = 0;
 window.requestAnimationFrame(update);
 
+const unlocks = {
+    DIVITIAE: 0
+}
+
+var unlocksUnlocked = {
+    DIVITIAE_UNLOCKED: false
+}
+
 function onCreateEnergy() {
 	energy += 1;
 }
@@ -19,5 +27,36 @@ function update() {
 
 
     document.getElementById("energyLabel").innerHTML = parseInt(energy);
+    updateUnlocks();
     window.requestAnimationFrame(update);
 }
+
+function onProductionClick() {
+    document.getElementById("productionBtn").innerHTML = "whatever";
+}
+
+function onMarketClick() {
+
+}
+
+function unlock(unlock) {
+    switch (unlock) {
+        case unlocks.DIVITIAE:
+            if (unlocksUnlocked.DIVITIAE_UNLOCKED) {
+                return;
+            }
+            unlocksUnlocked.DIVITIAE_UNLOCKED = true;
+            window.alert("You have unlocked DIVITIAE PHILOSOPHORUM! Sell your ENERGIA ARCANA and buy more complex production machines to become a great Alchemist!");
+            break;
+    }
+}
+
+function updateUnlocks() {
+    //money
+    if (energy >= 10) {
+        unlock(unlocks.DIVITIAE);
+    }
+    document.getElementById("moneyDiv").hidden = !unlocksUnlocked.DIVITIAE_UNLOCKED;
+    
+}
+
