ceb45494b87675979815f6b6a240544a1d7a0b66
DaJaBeCoding <38948265+DaJaBeCoding@users.noreply.github.com>
2021-09-23 15:45:06 +0200

    start with energy

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-﻿window.requestAnimationFrame(update);
-var mutter = 0.0;
-var vater = 0.0;
-var grossmutter = 0.0;
+﻿
 var lastTime = Date.now();
 var now = lastTime;
 var deltaT = 0;
+var energy = 0;
+window.requestAnimationFrame(update);
 
-function onMutter() {
-    mutter += 1.0;
+function onCreateEnergy() {
+	energy += 1;
 }
 
-function onVater() {
-    vater += 1.0;
-    mutter -= 10.0;
-}
-
-function onGrossmutter() {
-    grossmutter += 1.0;
-    mutter -= 500.0;
-}
 
 function update() {
     now = Date.now();
     deltaT = (now - lastTime) / 1000.0;
     lastTime = now;
 
-    vater += deltaT * grossmutter;
-    mutter += deltaT * vater;
 
-    document.getElementById("vaterBtn").disabled = mutter < 10
-    document.getElementById("grossmutterBtn").disabled = mutter < 500
-    document.getElementById("mutterLabel").innerHTML = parseInt(mutter);
-    document.getElementById("vaterLabel").innerHTML = parseInt(vater);
-    document.getElementById("grossmutterLabel").innerHTML = parseInt(grossmutter);
+
+
+    document.getElementById("energyLabel").innerHTML = parseInt(energy);
     window.requestAnimationFrame(update);
 }
